05d759179957b114f9df6e05c998d4fd4ecfe7f0
amineazizix <ejamen@wscripted.com>
2020-11-08 17:40:11 +0100

    Adding the portfolio slider

--- components/partials/Home/index.js
+++ components/partials/Home/index.js
@@ -6,8 +6,47 @@ export default function index() {
          <div className="container promise">
             <div className="row">
                <div className="col-sm-12 d-flex flex-column justify-content-center">
-                  <p className="promise-content mb-5 mt-5">G'day, I'm Amine Azizi, a <span className="focus-word">product guy</span> & a <span className="focus-word">data scientist</span>. I help teams worldwide get their products and ideas to the moon. I'm serious about creating amazing <span className="focus-word"> tools</span>, <span className="focus-word">practices</span>, and <span className="focus-word">open works</span> for creators, entrepreneurs, and creative teams.</p>
+                  <p className="promise-content mb-5 mt-5">G'day, I'm Amine Azizi, a <span className="focus-word">product guy</span> . I help teams worldwide get their ideas and products to the moon. I'm serious about creating amazing <span className="focus-word"> tools</span>, <span className="focus-word">practices</span>, and <span className="focus-word">open works</span> for creators, entrepreneurs, and creative teams.</p>
                   {/* <p className="promise-content mb-5">Wanna take your product from <span className="focus-word">ideation</span> to <span className="focus-word">creation</span> and finishing it with <span className="focus-word">monetization</span>?</p> */}
+               </div>
+            </div>
+         </div>
+
+         <div className="portfolio-blk">
+            <div className="company-slider-top">
+               <div className="slider-top-first">
+                  <div className="company-logo"><img src="/images/portfolio/legalplace.png" alt="Legalplace"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/lalchimiste.png" alt="L'alchimiste"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/place2swap.png" alt="Place2swap"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/wechsra.png" alt="Wechsra"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/skaalab.png" alt="Skaalab"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/edbridg.png" alt="Edbridg"/> </div>
+               </div>
+               <div className="slider-top-next">
+                  <div className="company-logo"><img src="/images/portfolio/legalplace.png" alt="Legalplace"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/lalchimiste.png" alt="L'alchimiste"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/place2swap.png" alt="Place2swap"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/wechsra.png" alt="Wechsra"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/skaalab.png" alt="Skaalab"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/edbridg.png" alt="Edbridg"/> </div>
+               </div>
+            </div>
+            <div className="company-slider-bottom">
+               <div className="slider-top-first">
+                  <div className="company-logo"><img src="/images/portfolio/familyphoto.png" alt="Family Photo"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/wscripted.png" alt="Wscripted"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/expresscanada.png" alt="Express Canada"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/torelf.png" alt="Torelf"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/imadrassa.png" alt="Imadrassa"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/tordot.png" alt="Tordot"/> </div>
+               </div>
+               <div className="slider-top-next">
+                  <div className="company-logo"><img src="/images/portfolio/familyphoto.png" alt="Family Photo"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/wscripted.png" alt="Wscripted"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/expresscanada.png" alt="Express Canada"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/torelf.png" alt="Torelf"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/imadrassa.png" alt="Imadrassa"/> </div>
+                  <div className="company-logo"><img src="/images/portfolio/tordot.png" alt="Tordot"/> </div>
                </div>
             </div>
          </div>
